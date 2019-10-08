ab5c230f9d9b43d0e5aa3203d013a78d8198dc52
gulara <gularah1997@gmail.com>
2019-10-08 18:03:26 +0400

    cssrefactoring

--- src/js/main/header.js
+++ src/js/main/header.js
@@ -1,7 +1,18 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//   }
+
+  $('#openNav').click(function () {
+    $('#mySidenav').css({
+      'width': '250px'
+    });
+  });
+  $('#closeNav').click(function () {
+    $('#mySidenav').css({
+      'width': '0'
+    });
+  });
