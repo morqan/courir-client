24c85858728798274ae3808e5849f48d043fb254
gulara <gularah1997@gmail.com>
2019-10-10 17:16:46 +0400

    cssrefactoring

--- src/js/main/header.js
+++ src/js/main/header.js
@@ -16,3 +16,29 @@
       'width': '0'
     });
   });
+
+  // $('#navbar-toggler').click(function () {
+  //   $("#navigation-menu").addClass("show");
+  // });
+
+
+//   $('#navbar-toggler').on('click', function () {
+//     $("#navigation-menu").toggleClass('show')
+// });
+
+// $(function () { 
+//   $('#navbar-toggler').on('click', function (e) {
+//       var menuItem = $( e.currentTarget );
+
+//       if (menuItem.attr( 'aria-expanded') === 'true') {
+//           $(this).attr( 'aria-expanded', 'false');
+//       } else {
+//           $(this).attr( 'aria-expanded', 'true');
+//       }
+//   });
+// });
+
+//   $('#navbar-toggler').on('click', function () {
+//    $("#navbar-toggler").attr("aria-expanded","false");
+// });
+
