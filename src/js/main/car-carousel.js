ef84af35bd85e6a0096d86506c010dc559e50751
gulara <gularah1997@gmail.com>
2019-10-15 17:10:30 +0400

    cssrefactoring

---
+++ src/js/main/car-carousel.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    // navText: [
+    //   "<i class='fa fa-caret-left'></i>",
+    //   "<i class='fa fa-caret-right'></i>"
+    // ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
--- src/js/main/datepicker.js
+++ src/js/main/datepicker.js
@@ -1,7 +1,23 @@
 $( function() {
     $( "#datepicker" ).datepicker({
-        dateFormat: "dd-mm-yy",
         duration: "fast",
         gotoCurrent: true,
+        closeText: "Bağla",
+	prevText: "&#x3C;Geri",
+	nextText: "İrəli&#x3E;",
+	currentText: "Bugün",
+	monthNames: [ "Yanvar","Fevral","Mart","Aprel","May","İyun",
+	"İyul","Avqust","Sentyabr","Oktyabr","Noyabr","Dekabr" ],
+	monthNamesShort: [ "Yan","Fev","Mar","Apr","May","İyun",
+	"İyul","Avq","Sen","Okt","Noy","Dek" ],
+	dayNames: [ "Bazar","Bazar ertəsi","Çərşənbə axşamı","Çərşənbə","Cümə axşamı","Cümə","Şənbə" ],
+	dayNamesShort: [ "B","Be","Ça","Ç","Ca","C","Ş" ],
+	dayNamesMin: [ "B","B","Ç","С","Ç","C","Ş" ],
+	weekHeader: "Hf",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" 
     });
 } );
