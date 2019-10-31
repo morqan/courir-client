ba7b530f066877f137480f8497cafa4600a17bfa
gulara <gularah1997@gmail.com>
2019-10-31 17:04:34 +0400

    car-pills-jas-refactoring

--- src/js/main/car-pills.js
+++ src/js/main/car-pills.js
@@ -1,22 +1,33 @@
 $('.hide-info').click(function () {
-    $('.info').css({
-      'display': 'none'
-    });
+  $('.info').css({
+    'display': 'none'
   });
-  $('.show-info').click(function () {
-    $('.info').css({
-      'display': 'block'
-    });
-
+});
+$('.show-info').click(function () {
+  $('.info').css({
+    'display': 'block'
   });
 
-  $('.hide-receiver').click(function () {
-    $('#receiver, #receiver-number').css({
-      'display': 'none'
-    });
+});
+
+$('.hide-receiver').click(function () {
+  $('#receiver, #receiver-number').css({
+    'display': 'none'
   });
-  $('.show-receiver').click(function () {
-    $('#receiver, #receiver-number').css({
-      'display': 'block'
-    });
+});
+$('.show-receiver').click(function () {
+  $('#receiver, #receiver-number').css({
+    'display': 'block'
   });
+});
+
+$('#pills-car-3-tab').click(function () {
+  $("#truck").prop( "checked", true );
+
+});
+
+$('#pills-car-2-tab').click(function () {
+  $("#evacuator").attr('checked', 'checked');
+
+});
+
