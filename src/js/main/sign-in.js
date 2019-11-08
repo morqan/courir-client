e80900072a5e7800bde7fb4699792077aed1627e
gulara <gularah1997@gmail.com>
2019-11-08 13:05:36 +0400

    js-refactoring

--- src/js/main/sign-in.js
+++ src/js/main/sign-in.js
@@ -1,44 +1,72 @@
-$(document).ready(function () {
-
-    $('#sign-in-form').submit(function (e) {
-        e.preventDefault();
-
-    });
-
-    $("#phoneNumber").focus(function () {
-        $(this).addClass("inputErrorBorder");
-    });
-
-
-    $("#phoneNumber").keypress(function () {
-
-        var phoneNumber = $('#phoneNumber').val();
-        phoneNumber = phoneNumber.replace(/[^0-9\.]+/g, '');
-        if (phoneNumber.length != 11) {
-            $('#phoneNumber').addClass("inputErrorBorder");
-        } else if (phoneNumber.length == 11) {
-            $('#phoneNumber').removeClass("inputErrorBorder");
-        }
-
-    });
-
-});
-
-
 // $(document).ready(function () {
 
 //     $('#sign-in-form').submit(function (e) {
 //         e.preventDefault();
-//         $('#phoneNumber').addClass("inputErrorBorder");
 
-//         if ($("#membership-checkbox[type=checkbox]").is( 
-//             ":checked")) { 
+//     });
+
+//     $("#phoneNumber").focus(function () {
+//         $(this).addClass("inputErrorBorder");
+//     });
 
 
+//     $("#phoneNumber").keypress(function () {
 
-//           } else { 
-//             $(".account-sign-in-cover2__label").after('<span class="error">This field is required</span>');
-//           } 
+//         var phoneNumber = $('#phoneNumber').val();
+//         phoneNumber = phoneNumber.replace(/[^0-9\.]+/g, '');
+//         if (phoneNumber.length != 11) {
+//             $('#phoneNumber').addClass("inputErrorBorder");
+//         } else if (phoneNumber.length == 11) {
+//             $('#phoneNumber').removeClass("inputErrorBorder");
+//         }
+
 //     });
 
 // });
+
+
+$(document).ready(function () {
+
+    $('#sign-in-form').submit(function (e) {
+        e.preventDefault();
+        $('#phoneNumber').addClass("inputErrorBorder");
+
+        $('#membership-checkbox[type="checkbox"]').click(function () {
+            if ($(this).prop("checked") == true) {
+                $('.membership-span').removeClass("inputErrorBorder");
+            } else {
+                $('.membership-span').addClass("inputErrorBorder");
+            }
+
+        });
+
+        if ($("#membership-checkbox").prop("checked") == false) {
+            $('.membership-span').addClass("inputErrorBorder");
+
+        }
+
+
+
+    });
+    // $("#phoneNumber").focus(function () {
+    //     $(this).addClass("inputErrorBorder");
+    // });
+
+
+    $("#phoneNumber").keydown(function () {
+
+        var phoneNumber = $('#phoneNumber').val();
+        intRegex = /[0-9 -()+]+$/;
+        // phoneNumber = phoneNumber.replace(/[^0-9\.]+/g, '');
+        if (phoneNumber.length != 11  || (!intRegex.test(phoneNumber))) {
+           
+            $('#phoneNumber').addClass("inputErrorBorder");
+        } else  {
+        
+            $('#phoneNumber').removeClass("inputErrorBorder");
+        } 
+
+    });
+
+  
+});
