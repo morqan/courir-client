f7726db2b8354a72e9f297c078b2f42834141b6a
gulara <gularah1997@gmail.com>
2019-11-19 17:02:51 +0400

    js-refactoring

--- src/js/main/date.js
+++ src/js/main/date.js
@@ -7,8 +7,22 @@ $(function () {
     var date = today.getDate();
     var dateTime = date + ' ' + month;
     var tomorrow = date + 1 + ' ' + month;
+    var timeVal1 = $("input[name='time1']:checked").val();
+    var timeVal2 =$("input[name='time2']:checked").val();
+
+    var monthStr = today.getMonth() + 1;
+    var year = today.getFullYear();
+
+    var todayStr = date + "/" + monthStr + "/" + year;
+    var tomorrowStr = date + 1 + "/" + monthStr + "/" + year;
+
+    var todayInput = document.getElementById("today");
+    todayInput.value = todayStr + " " + timeVal1;
+
+    var tomorrowInput = document.getElementById("tomorrow");
+    tomorrowInput.value =tomorrowStr + " " + timeVal2;
 
     $("#today-span").html(dateTime);
-     $("#tomorrow-span").html(tomorrow);
-  
+    $("#tomorrow-span").html(tomorrow);
+
 });
