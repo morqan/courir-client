36bea743e646bdfd8a0be8be6aeff10f30397453
gulara <gularah1997@gmail.com>
2019-11-26 17:05:29 +0400

    header-html-js landing-4-html

---
+++ src/js/main/header.js
@@ -0,0 +1,11 @@
+$(window).on('scroll', function() {
+  
+    if( $(".navbar").hasClass("scrolled")){
+       
+        $('.header__language2 , .header__toggler2, .header__language:after').addClass('header-scroll');
+      
+    }else{
+        $('.header__language2 , .header__toggler2, .dropdown-toggle:after').removeClass('header-scroll');
+    }
+
+});
