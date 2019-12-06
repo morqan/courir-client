9d1351dd6547402bdeb43b63fa1f1d09ca549715
gulara <gularah1997@gmail.com>
2019-12-06 17:04:29 +0400

    scroll-js

---
+++ src/js/main/scroll.js
@@ -0,0 +1,30 @@
+// SCROLL
+
+// $(document).ready(function () {
+
+//     var $scrollheight = $('scrollbar1'),
+//     scrollBottom = 490;
+
+//     var $window = $(window),
+//         previousScrollTop = 0,
+//         scrollLock = true;
+
+//     $window.scroll(function (event) {
+     
+//         if (scrollLock) {
+//             $window.scrollTop(previousScrollTop);
+//         }
+//         previousScrollTop = $window.scrollTop();
+//     });
+   
+  
+//     $(".scrollbar1").scroll(function () {
+//         if ( $scrollheight.scrollTop(scrollBottom)) {        
+//             scrollLock = false;
+//         } else {
+//             scrollLock = true;
+//         }
+//     })
+
+  
+// });
