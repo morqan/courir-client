5af8b13120210a8179c39869516fd26bd43e7701
gulara <gularah1997@gmail.com>
2019-12-13 15:07:14 +0400

    intl-input-datepicker

--- src/js/main/intlTelInput.js
+++ src/js/main/intlTelInput.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
   var input = document.querySelector("#phoneNumber"),
     errorMsg = document.querySelector("#error-msg"),
     validMsg = document.querySelector("#valid-msg");
+      //   // initialise plugin
   var iti = window.intlTelInput(input, {
     utilsScript: "libs/intl-tel-input/build/js/utils.js"
   });
+  
+  //   // here, the index maps to the error code returned from getValidationError - see readme
   var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
 
   // //   // FOR ACCOUNT-SIGN-IN-COVER2.HTML
@@ -20,14 +23,16 @@ $(document).ready(function () {
     // separateDialCode: true,
 
   });
-  var countryDialCode = $("#phoneNumber").intlTelInput("getSelectedCountryData").dialCode;
+   countryDialCode = $("#phoneNumber").intlTelInput("getSelectedCountryData").dialCode;
+ 
   $('#phoneNumber').val('+' + countryDialCode);
   input.addEventListener('countrychange', function () {
 
-   var countryDialCode = $("#phoneNumber").intlTelInput("getSelectedCountryData").dialCode;
+    var countryDialCode = $("#phoneNumber").intlTelInput("getSelectedCountryData").dialCode;
     console.log(countryDialCode);
     $('#phoneNumber').val('+' + countryDialCode);
-    $("#dialCode").val('+' + countryDialCode);
+    
+
     if (countryDialCode !== "994") {
       $("#use-sms").attr("disabled", "disabled");
     } else if (countryDialCode === "994") {
@@ -38,15 +43,14 @@ $(document).ready(function () {
 
 
 
-  //   // here, the index maps to the error code returned from getValidationError - see readme
-
-
-  //   // initialise plugin
 
 
 
 
- let reset = function () {
+
+
+
+  let reset = function () {
     input.classList.remove("error");
     errorMsg.innerHTML = "";
     errorMsg.classList.add("valid-hide");
@@ -61,7 +65,7 @@ $(document).ready(function () {
         validMsg.classList.remove("valid-hide");
       } else {
         input.classList.add("error");
-       let errorCode = iti.getValidationError();
+        let errorCode = iti.getValidationError();
         errorMsg.innerHTML = errorMap[errorCode];
         errorMsg.classList.remove("valid-hide");
       }
@@ -69,13 +73,14 @@ $(document).ready(function () {
   });
 
   $('#use-sms').click(function () {
+    console.log( $('#phoneNumber').val());
     reset();
     if (input.value.trim()) {
       if (iti.isValidNumber()) {
         validMsg.classList.remove("valid-hide");
       } else {
         input.classList.add("error");
-       let errorCode = iti.getValidationError();
+        let errorCode = iti.getValidationError();
         errorMsg.innerHTML = errorMap[errorCode];
         errorMsg.classList.remove("valid-hide");
       }
